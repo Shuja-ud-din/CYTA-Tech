c9d5d6df384eee774d66845b2dc1ed058d417765
Shuja ud Din <shuja1339@gmail.com>
2023-08-30 13:46:39 +0500

    warning1res

--- src/components/Home/OurProcess/OurProcess.js
+++ src/components/Home/OurProcess/OurProcess.js
@@ -1,5 +1,4 @@
-import React, { useContext, useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useContext } from 'react'
 import { DataContext } from '../../../context/AppData'
 import ProcessTab from './ProcessTab';
 
--- src/components/Home/OurProcess/ProcessTab.js
+++ src/components/Home/OurProcess/ProcessTab.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import btnArrow from '../../../assets/images/btn-arrow.png'
 import arrowC2 from '../../../assets/images/arrow-c2.png'
 import { NavLink } from 'react-router-dom'
